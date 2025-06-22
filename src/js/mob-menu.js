a25587e339213c8a0d789610d97ca3449ccc4999
Olga Belaya <belayaolga188@gmail.com>
2025-06-22 16:10:13 +0300

    add mob menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -10,25 +10,22 @@
 //   burgerMenuEl.dataset.visible = 'close';
 // });
 
-// const menu = document.getElementById('mobile-menu');
-// const overlay = document.getElementById('menu-overlay');
-// const openBtn = document.querySelector('.menu-button');
-// const closeBtn = document.getElementById('menu-close');
+const openBtn = document.getElementById('menu-open');
+const closeBtn = document.getElementById('menu-close');
+const menu = document.getElementById('mobile-menu');
+const overlay = document.getElementById('menu-overlay');
 
-// openBtn.addEventListener('click', () => {
-//   menu.classList.add('is-open');
-//   overlay.classList.add('is-visible');
-//   document.body.classList.add('menu-open');
-// });
+openBtn.addEventListener('click', () => {
+  menu.classList.add('is-open');
+  overlay.classList.add('is-visible');
+  document.body.classList.add('menu-open');
+});
 
-// closeBtn.addEventListener('click', () => {
-//   menu.classList.remove('is-open');
-//   overlay.classList.remove('is-visible');
-//   document.body.classList.remove('menu-open');
-// });
+closeBtn.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
 
-// overlay.addEventListener('click', () => {
-//   menu.classList.remove('is-open');
-//   overlay.classList.remove('is-visible');
-//   document.body.classList.remove('menu-open');
-// });
+function closeMenu() {
+  menu.classList.remove('is-open');
+  overlay.classList.remove('is-visible');
+  document.body.classList.remove('menu-open');
+}
